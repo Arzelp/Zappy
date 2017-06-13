2bd4b5de6683379dec9e177122d2c9dbc3e685c5
Paskal <paskal.arzel@epitech.eu>
2017-06-13 17:21:40 +0200

    --help c bn g fini

--- client/src/main.c
+++ client/src/main.c
@@ -1,16 +1,20 @@
 /*
 ** main.c for lol in /home/tetra/rendu/psu/PSU_2016_zappy/server
-** 
+**
 ** Made by Arthur Josso
 ** Login   <arthur.josso@epitech.eu>
-** 
+**
 ** Started on  Tue Jun  6 13:55:14 2017 Arthur Josso
-** Last update Tue Jun  6 13:55:33 2017 Arthur Josso
+** Last update Tue Jun 13 17:15:12 2017 Paskal Arzel
 */
+
+#include "core.h"
 
 int	main(int ac, char **av)
 {
-  (void)ac;
-  (void)av;
+  t_client	client;
+
+  if (ac == 1 || !strcmp(av[1], "--help"))
+    return (help());
   return (0);
 }
