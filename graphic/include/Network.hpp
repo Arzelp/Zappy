ec3268787f480ddddd0915106cc51c23289930bd
Frederic Oddou <fredoddou@fredoddou-arch.localdomain>
2017-06-23 11:23:59 +0200

    network

--- graphic/include/Network.hpp
+++ graphic/include/Network.hpp
@@ -1,21 +1,23 @@
 //
-// Network.hpp for network in /graphic/include/
+// Network.hpp for Network in /graphic/include/
 //
 // Made by Frederic Oddou
 // Login   <frederic.oddou@epitech.eu>
 //
 // Started on  Tue Jun 20 08:49:32 2017 Frederic Oddou
-// Last update Tue Jun 20 11:08:44 2017 Frederic Oddou
+// Last update Fri Jun 23 11:19:25 2017 Frederic Oddou
 //
 
 #pragma once
 
 #include <iostream>
 #include <queue>
+#include <thread>
+#include <mutex>
 #include <sys/socket.h>
 #include <netdb.h>
 
-class NetWork
+class Network
 {
 private:
   std::string			_host;
@@ -24,16 +26,21 @@ private:
   struct sockaddr_in		_socketPort;
   std::queue<std::string>	_queue;
   int				_ipAddr[4];
+  std::thread			_thread;
+  std::mutex			_mutex;
+  bool				_endThread;
 
 private:
   bool				InitTcpSocket(void);
   void				InitPortSocket(void);
   bool				GetIp(void);
+  void	 			ReceiveMsg();
 
 public:
-  NetWork(const std::string &host, int port);
-  ~NetWork();
-  bool				SendMsg(std::string &str);
-  bool	 			ReceiveMsg(void);
+  Network(const std::string &host, int port);
+  ~Network();
+  bool				SendMsg(std::string str);
   std::string			GetQueue(void);
+  void				ReceiveStop();
+  void				ReceiveStart();
 };
