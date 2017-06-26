4407eff03a4e67287f5c740e2ddf1fb8012aa541
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-06-26 15:27:14 +0200

    test

--- client/src/connect/receive_msg.c
+++ client/src/connect/receive_msg.c
@@ -5,7 +5,7 @@
 ** Login   <frederic.oddou@epitech.eu>
 **
 ** Started on  Fri Jun 16 13:34:54 2017 Frederic Oddou
-** Last update Fri Jun 16 13:46:47 2017 Frederic Oddou
+** Last update Mon Jun 26 15:25:13 2017 arnaud.alies
 */
 
 #include <sys/socket.h>
@@ -15,6 +15,27 @@
 
 bool		receive_msg(char *str, size_t len)
 {
+  char          c;
+  ssize_t       size;
+  size_t	x;
+
+  x = 0;
+  while ((size = recv(g_core->socket_fd, &c, 1, 0)) > 0
+	 && c != '\n'
+	 && (x + 1) < len)
+    {
+      str[x] = c;
+      x += 1;
+    }
+  str[x] = '\0';
+  if (size == -1)
+    {
+      perror("recv");
+      return (false);
+    }
+  return (true);
+
+  /*
   ssize_t	size;
 
   bzero(str, len);
@@ -26,4 +47,5 @@ bool		receive_msg(char *str, size_t len)
   if (size > 0 && str[size - 1] == '\n')
     str[size - 1] = '\0';
   return (true);
+  */
 }
